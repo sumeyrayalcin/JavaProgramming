9383f6f02ed3e45e65d38f5f820b4f31a9ccd96f
Sumeyra <smyryalcin@gmail.com>
2022-12-14 14:50:54 +0000

    created parking  class

---
+++ src/day38_Inheritance/car/ParkingLot.java
@@ -0,0 +1,8 @@
+package day38_Inheritance.car;
+
+public class ParkingLot {
+    public static void main(String[] args) {
+        Toyota toyota = new Toyota("Camry",2020,4465,"red",5859);
+
+    }
+}
