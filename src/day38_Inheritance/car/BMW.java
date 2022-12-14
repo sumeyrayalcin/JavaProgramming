15d7edebdd69d288666ee2d01e96bee2e8710755
Sumeyra <smyryalcin@gmail.com>
2022-12-14 14:28:24 +0000

    created BMW class uplteded tesla class

---
+++ src/day38_Inheritance/car/BMW.java
@@ -0,0 +1,9 @@
+package day38_Inheritance.car;
+
+public class BMW extends Car{
+    public BMW( String model, int year, double price, String color, int miles) {
+        super("BMW", model, year, price, color, miles);
+    }
+
+
+}
--- src/day38_Inheritance/car/Tesla.java
+++ src/day38_Inheritance/car/Tesla.java
@@ -1,4 +1,19 @@
 package day38_Inheritance.car;
 
-public class Tesla {
+public class Tesla extends Car{
+
+
+    public Tesla(String model, int year, double price, String color, int miles) {
+        super("Tesla", model, year, price, color, miles);
+    }
+
+
+    public void autoPilot(){
+        System.out.println(brand+" "+model+" in autopilot mode");
+    }
+
+
+    public void start() {
+        System.out.println("Say \"Start\" to start "+brand+" "+model);
+    }
 }
