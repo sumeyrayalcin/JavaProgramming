c192e67986f5b77493c6664b6d39d49a07fa96fe
Sumeyra <smyryalcin@gmail.com>
2022-12-16 12:20:04 +0000

    day 39 sub of  Employee class of Developer  class done

--- src/day39_Recap/cydeo/Developer.java
+++ src/day39_Recap/cydeo/Developer.java
@@ -1,4 +1,24 @@
 package day39_Recap.cydeo;
 
-public class Developer {
+public class Developer extends Employee{
+    public Developer(String name, int age, char gender, int employeeId, String jobTitle, double salary) {
+        super(name, age, gender, employeeId, jobTitle, salary);
+    }
+
+    public void fixingBugs(){
+        System.out.println( getJobTitle() + getName() + " is fixing bugs");
+    }
+
+    @Override
+    public void work() {
+        System.out.println( getJobTitle() + getName() + " is developing the application");
+    }
 }
+
+
+/*
+4. Create a sub class of Employee named Developer
+            Override the work method
+            Extra methods:
+                fixingBugs()
+ */
