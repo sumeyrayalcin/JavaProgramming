f3afa2bfbadda15c51dd785969da4393b44d9508
Sumeyra <smyryalcin@gmail.com>
2022-12-16 21:58:39 +0000

    candy class created

---
+++ src/day35_Encapsulation/CandyFactory.java
@@ -0,0 +1,15 @@
+package day35_Encapsulation;
+
+public class CandyFactory {
+    public static void main(String[] args) {
+        Candy candy = new Candy("M&M", 60, 1.5, false);
+    }
+}
+
+/*
+reate a class named CandyFactory
+Create an ArrayList of candies
+Add five objects of candies
+use for each loop to print the brand and price of each
+candy
+ */
