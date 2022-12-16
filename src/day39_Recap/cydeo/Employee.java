54b56d29f5abf27284ce6bbba61c1a8d07878fb1
Sumeyra <smyryalcin@gmail.com>
2022-12-16 11:56:06 +0000

    day 39 sub of person employee class done

--- src/day39_Recap/cydeo/Employee.java
+++ src/day39_Recap/cydeo/Employee.java
@@ -1,4 +1,65 @@
 package day39_Recap.cydeo;
 
-public class Employee {
+public class Employee extends Person {
+    private int employeeId;
+    private String jobTitle;
+    private double salary;
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public Employee(String name, int age, char gender, int employeeId, String jobTitle, double salary) {
+        super(name, age, gender);
+        setEmployeeId(employeeId);
+        setSalary(salary);
+        setGender(gender);
+    }
+
+    public void work(){
+        System.out.println( getName() + " is working");
+    }
+
+    public String toString() {
+        return "Employee{" +
+                "name='" + getName() + '\'' +
+                ", age=" + getAge() +
+                ", gender=" + getGender() +
+                "employeeId=" + employeeId +
+                ", jobTitle='" + jobTitle + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
 }
+
+
+/*
+2. Create a sub class of Person named Employee:
+            Extra variables:
+                employeeId, jobTitle, salary
+            Encapsulate all the fields
+            Add a constructor to set all the fields
+            Methods:
+                work()
+                toString()
+ */
