217567202d40854274123f1b69d165ea9bfc2537
Sumeyra <smyryalcin@gmail.com>
2022-12-16 12:16:40 +0000

    day 39 sub of  Employee class of Tester class done

--- src/day39_Recap/cydeo/Tester.java
+++ src/day39_Recap/cydeo/Tester.java
@@ -1,4 +1,24 @@
 package day39_Recap.cydeo;
 
-public class Tester {
+public class Tester extends Employee{
+    public Tester(String name, int age, char gender, int employeeId, String jobTitle, double salary) {
+        super(name, age, gender, employeeId, jobTitle, salary);
+    }
+
+    public void createTicket(){
+        System.out.println(getJobTitle() + getName() + " is creating ticket");
+    }
+
+    @Override
+    public void work() {
+        System.out.println( getJobTitle() + getName() + " is testing the application");
+    }
 }
+
+
+/*
+3. Create a sub class of Employee named Tester
+            Override the work method
+            Extra methods:
+                createTicket()
+ */
