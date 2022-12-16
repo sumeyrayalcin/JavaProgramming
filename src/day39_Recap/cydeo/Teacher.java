d25ae6a56595f6b8f59742022a273094b9b404dd
Sumeyra <smyryalcin@gmail.com>
2022-12-16 12:23:22 +0000

    day 39 sub of  Employee class of Teacher
     class done

--- src/day39_Recap/cydeo/Teacher.java
+++ src/day39_Recap/cydeo/Teacher.java
@@ -1,4 +1,20 @@
 package day39_Recap.cydeo;
 
-public class Teacher {
+public class Teacher extends Employee{
+
+
+    public Teacher(String name, int age, char gender, int employeeId, String jobTitle, double salary) {
+        super(name, age, gender, employeeId, jobTitle, salary);
+    }
+        @Override
+        public void work(){
+            System.out.println(getJobTitle() + getName() + " is teaching");
+
+    }
 }
+
+
+/*
+5. Create a sub class of Employee named Teacher
+            Override the work method
+ */
