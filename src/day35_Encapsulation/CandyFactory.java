3796289257fca2ab085b8d3aeac9ab0c75cc2fb4
Sumeyra <smyryalcin@gmail.com>
2022-12-26 13:30:42 +0000

    candy class created

--- src/day35_Encapsulation/CandyFactory.java
+++ src/day35_Encapsulation/CandyFactory.java
@@ -1,8 +1,24 @@
 package day35_Encapsulation;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class CandyFactory {
     public static void main(String[] args) {
-        Candy candy = new Candy("M&M", 60, 1.5, false);
+        ArrayList<Candy> candies = new ArrayList<>();
+        candies.addAll(Arrays.asList(
+                new Candy("M&M", 60, 1.5, false),
+                new Candy("M&M", 50, 2.5, false),
+                new Candy("M&M", 43, 4.5, false),
+                new Candy("M&M", 32, 5.5, false),
+                new Candy("M&M", 21, 6.5, false)
+        ));
+
+        for (Candy candy: candies) {
+            System.out.println( candy.getBrand() +" " + candy.getPrice());
+            
+        }
+
     }
 }
 
