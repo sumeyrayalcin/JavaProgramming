ecf454e84fae312cad96bd93e8bda95afc4b0426
Sumeyra <smyryalcin@gmail.com>
2022-12-30 12:33:53 +0000

    day42 ThrowKeyword created

--- src/day42_Exceptions/DisadvantageOfThrowsKeyword2.java
+++ src/day42_Exceptions/DisadvantageOfThrowsKeyword2.java
@@ -1,7 +1,7 @@
 package day42_Exceptions;
 
 public class DisadvantageOfThrowsKeyword2 {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         // System.out.println("Hello");
 
         method1();
