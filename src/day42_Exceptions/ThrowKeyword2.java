50422ce0736afb40a942a4c47048ebbe44a6f9c2
Sumeyra <smyryalcin@gmail.com>
2022-12-30 12:45:15 +0000

    day42 ThrowKeyword done

---
+++ src/day42_Exceptions/ThrowKeyword2.java
@@ -0,0 +1,16 @@
+package day42_Exceptions;
+
+import day39_Recap.shape.Rectangle;
+
+import java.util.NoSuchElementException;
+
+public class ThrowKeyword2 {
+    public static void main(String[] args) {
+      throw new NoSuchElementException("No such element");
+       // throw new InterruptedException();
+
+      //  throw new Rectangle(5,7);
+
+
+    }
+}
