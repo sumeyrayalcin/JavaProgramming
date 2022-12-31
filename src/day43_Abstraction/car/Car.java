e7fb96e123a26afe90fcb1576c011f9aacb9985f
Sumeyra <smyryalcin@gmail.com>
2022-12-31 21:57:41 +0000

    day43 Car class updated

--- src/day43_Abstraction/car/Car.java
+++ src/day43_Abstraction/car/Car.java
@@ -1,6 +1,6 @@
 package day43_Abstraction.car;
 
-public class Car {
+public abstract class Car {
     private final String brand, model;
     private String color;
     private final int year;
@@ -47,6 +47,11 @@ public class Car {
             throw new RuntimeException("invalid price "+ price);
         }
     }
+
+    public void stop(){
+        System.out.println("Press the brake");
+    }
+    public abstract void start();
 
     @Override
     public String toString() {
