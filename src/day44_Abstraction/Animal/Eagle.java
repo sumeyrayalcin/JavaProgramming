7572bf5639c011c3786f7f5dcc9db1cc07c0e34e
Sumeyra <smyryalcin@gmail.com>
2023-01-10 13:18:21 +0000

    eagle class with implement and extend task1

---
+++ src/day44_Abstraction/Animal/Eagle.java
@@ -0,0 +1,23 @@
+package day44_Abstraction.Animal;
+
+public class Eagle extends Animal implements WildAnimal, Flyable{
+    public Eagle(String name, String breed, char gender, int age, String size, String color) {
+        super(name, breed, gender, age, size, color);
+    }
+
+    @Override
+    public void eat() {
+        System.out.println(getName() + " eats snake");
+    }
+
+    @Override
+    public void hunt() {
+        System.out.println(getName()+ " hunts snake");
+
+    }
+
+    @Override
+    public void fly() {
+        System.out.println(getName() +"  can fly 50km/h");
+    }
+}
