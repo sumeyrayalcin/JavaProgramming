abcaa5cf8f68013032a450d81a9617ab257ec842
Sumeyra <smyryalcin@gmail.com>
2023-01-10 13:09:25 +0000

    inteface third class task1

--- src/day44_Abstraction/Animal/Cat.java
+++ src/day44_Abstraction/Animal/Cat.java
@@ -1,6 +1,6 @@
 package day44_Abstraction.Animal;
 
-public final class Cat extends Animal {
+public final class Cat extends Animal implements Playable {
     public Cat(String name, String breed, char gender, int age, String size, String color) {
         super(name, breed, gender, age, size, color);
     }
@@ -12,5 +12,10 @@ public final class Cat extends Animal {
 
     public void meow() {
         System.out.println(getName() + " is meowing");
+    }
+
+    @Override
+    public void play() {
+
     }
 }
