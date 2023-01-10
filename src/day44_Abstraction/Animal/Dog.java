73bb10061c4ad185f264aacbcc83f82cd17bd24f
Sumeyra <smyryalcin@gmail.com>
2023-01-10 13:14:42 +0000

    dog class task1

---
+++ src/day44_Abstraction/Animal/Dog.java
@@ -0,0 +1,20 @@
+package day44_Abstraction.Animal;
+
+public class Dog extends Animal implements Playable{
+    public Dog(String name, String breed, char gender, int age, String size, String color) {
+        super(name, breed, gender, age, size, color);
+    }
+
+    @Override
+    public void eat() {
+        System.out.println(getName() + " is eating pizza");
+    }
+
+    public void bark(){
+        System.out.println( getName() + "is barking");
+    }
+
+    public void play(){
+        System.out.println(getName()+ " is playing with ball");
+    }
+}
